19895cd264f231ec93875a36d06213e1b6a07bdd
Tiago Miguel Cunha <tiago.miguel.cunha@celfocus.com>
2022-07-28 22:41:01 +0100

    Add home pae layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,27 @@
 import type { NextPage } from 'next';
 import { trpc } from '../utils/trpc';
-import {PageLayout} from '../components/PageLayout';
+import { PageLayout } from '../components/PageLayout';
 
 const Home: NextPage = () => {
   const hello = trpc.useQuery(['example.hello', { text: 'from tRPC' }]);
 
-  return <PageLayout>{"teste"}</PageLayout>;
+  return (
+    <PageLayout>
+      <div className='flex flex-col h-full'>
+        <div className='h-16 flex justify-center items-center'>
+          <h1 className='text-white text-lg'>Cards of the day!</h1>
+        </div>
+        <div className='flex flex-1'>
+          <div className='w-1/5 bg-purple'>
+            filter area
+          </div>
+          <div className='flex-1 bg-white'>
+            flash card
+          </div>
+        </div>
+      </div>
+    </PageLayout>
+  );
 };
 
 export default Home;
